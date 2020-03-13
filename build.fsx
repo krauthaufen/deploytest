6354fe39d03dc8e38903f1d3fd6100629eb203ed
Georg Haaser <krauthaufen@awx.at>
2020-03-13 13:02:13 +0100

    sync

--- build.fsx
+++ build.fsx
@@ -7,15 +7,20 @@ open Fake.Api
 open Fake.DotNet
 
 Target.create "Default" (fun _ ->
-    let pkg = "Aardvark.Base.Essentials.5.0.3.nupkg"
-    pkg |> DotNet.nugetPush (fun p ->   
-        { p with
-            PushParams = 
-                { p.PushParams with
-                    Source = Some "github"
-                }
-        }
-    )
+    let env = Environment.GetEnvironmentVariable "GITHUB_PACKAGE_TOKEN"
+    Trace.traceImportantfn "TOKEN: %A %A" env.Length env
+    //let pkg = "Aardvark.Base.Essentials.5.0.3.nupkg"
+
+    //Process.shellExec
+
+    //pkg |> DotNet.nugetPush (fun p ->   
+    //    { p with
+    //        PushParams = 
+    //            { p.PushParams with
+    //                Source = Some "github"
+    //            }
+    //    }
+    //)
 
 )
 
