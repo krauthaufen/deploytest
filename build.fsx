9b41098aeb451f4676cfb822349a8dc5b794b606
Georg Haaser <krauthaufen@awx.at>
2020-03-13 13:40:02 +0100

    another test

--- build.fsx
+++ build.fsx
@@ -2,12 +2,17 @@
 #load ".fake/build.fsx/intellisense.fsx"
 
 open System
+open System.IO
 open Fake.Core
 open Fake.Api
 open Fake.DotNet
 
 Target.create "Default" (fun _ ->
     let env = Environment.GetEnvironmentVariable "GITHUB_PACKAGE_TOKEN"
+
+    let cfg = File.ReadAllText("nuget.config").Replace("GITHUB_PACKAGE_TOKEN", env)
+    File.WriteAllText("nuget.config", cfg)
+
 
     Trace.traceImportantfn "TOKEN: %A %A" env.Length env
     let pkg = "Aardvark.Base.Essentials.5.0.3.nupkg"
@@ -16,8 +21,7 @@ Target.create "Default" (fun _ ->
         { p with
             PushParams = 
                 { p.PushParams with
-                    Source = Some "https://vrvis.myget.org/F/testfeed/api/v3/index.json"
-                    ApiKey = Some env
+                    Source = Some "github"
                 }
         }
     )
